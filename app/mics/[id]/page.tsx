5931d5dab26550da96a7df070b50a3a4c9ac1a0d
Kari Burt <kandreahburt@gmail.com>
2024-05-12 15:20:15 -0400

    working on static params

--- app/mics/[id]/page.tsx
+++ app/mics/[id]/page.tsx
@@ -3,6 +3,10 @@
 import { MicDetailPage2 } from '@/components/micdetailpage/MicDetailPage';
 import { MicDetailContextProvider } from '@/lib/context/MicDetailContext';
 
+export function generateStaticParams() {
+  return [{ id: '1' }, { id: '2' }, { id: '3' }];
+}
+
 export default function Page() {
   return (
     <MicDetailContextProvider>
