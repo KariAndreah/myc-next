aea287d43f38c7d1e04a72eecd356a735a7df0ea
Kari Burt <kandreahburt@gmail.com>
2024-05-12 15:27:23 -0400

    fixing static site

--- app/mics/[id]/page.tsx
+++ app/mics/[id]/page.tsx
@@ -1,14 +1,19 @@
+'use client';
+
+import { Suspense } from 'react';
 import { MicDetailPage2 } from '@/components/micdetailpage/MicDetailPage';
 import { MicDetailContextProvider } from '@/lib/context/MicDetailContext';
 
-export function generateStaticParams() {
-  return [{ id: '1' }, { id: '2' }, { id: '3' }];
-}
+// export function generateStaticParams() {
+//   return [{ id: '1' }, { id: '2' }, { id: '3' }];
+// }
 
 export default function Page() {
   return (
-    <MicDetailContextProvider>
-      <MicDetailPage2 />
-    </MicDetailContextProvider>
+    <Suspense>
+      <MicDetailContextProvider>
+        <MicDetailPage2 />
+      </MicDetailContextProvider>
+    </Suspense>
   );
 }
