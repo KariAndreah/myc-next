e1f817317f2e0b9162f131c71c103a181c0149c6
Kari Burt <kandreahburt@gmail.com>
2024-05-15 23:15:15 -0400

    still trying

--- components/filter/MobileFilterButton.tsx
+++ components/filter/MobileFilterButton.tsx
@@ -2,6 +2,7 @@ import { TbSearch } from 'react-icons/tb';
 import { Button, Modal } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import MobileFilter from './MobileFilter';
+import '@mantine/core/styles.css';
 
 const MobileFilterButton = () => {
   const [opened, { open, close }] = useDisclosure(false);
--- components/pagination/ChatPagination.tsx
+++ components/pagination/ChatPagination.tsx
@@ -4,6 +4,7 @@ import { Button, Group } from '@mantine/core';
 // import { useRouter } from 'next/navigation';
 import { useContext } from 'react';
 import { MicListingContext } from '@/lib/context/MicListingContext';
+import '@mantine/core/styles.css';
 
 const ChatPagination = ({ currentPage, totalPages }: ChatPaginationProps) => {
   const { params } = useContext(MicListingContext);
