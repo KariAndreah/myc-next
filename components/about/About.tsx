ecdad4067c0995d6d5ff56a8ac91ea7988a7d3f5
Kari Burt <kandreahburt@gmail.com>
2024-06-04 05:25:40 -0400

    some more touches

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -2,6 +2,9 @@
 
 import '@mantine/core/styles.css';
 import { Card } from '@mantine/core';
+import Link from 'next/link';
+import { SiCashapp } from 'react-icons/si';
+import { IoLogoVenmo } from 'react-icons/io5';
 import PageLayout from '../pagelayout/PageLayout';
 
 const About = () => (
@@ -13,14 +16,29 @@ const About = () => (
         <p className="pt-2">
           This website provides the location of open mics throughout the 5 boroughs of NYC. Using
           this website, you can find open mics based on the borough, day, time and most importantly
-          whether theya re free.
+          whether they are <span className="font-bold">FREE</span>.
         </p>
         <h3 className="font-bold text-xl pt-6">How can I support the app?</h3>
         <p className="pt-2">
-          {' '}
-          If you are enjoying the app my Venmo, Cashapp, are below. Especially if you actually make
-          it{' '}
+          If you are enjoying the app my Venmo and Cashapp are below. Especially if you actually
+          make! it!
         </p>
+        <div className="flex flex-row p-3 justify-center items-center text-blue-700 font-bold gap-4">
+          <a
+            className="flex flex-row bg-blue-500 p-1 border-solid border-2 border-grey-700 rounded-lg items-center"
+            href="https://cash.app/$KariBurt"
+          >
+            <SiCashapp size="25px" color="white" />
+            <p className="p-2 text-white">Cashapp</p>
+          </a>
+          <a
+            className="flex flex-row bg-blue-500 p-1 border-solid border-2 border-grey-700 rounded-lg items-center"
+            href="https://venmo.com/u/kariandreah"
+          >
+            <IoLogoVenmo size="25px" color="white" />
+            <p className="p-2 text-white">Venmo</p>
+          </a>
+        </div>
         <h3 className="font-bold text-xl pt-6">Some mics are missing!</h3>
         <p className="pt-2">
           This website is built based on what mics we know of. If you know of a mic that should be
@@ -32,8 +50,14 @@ const About = () => (
 
         <h3 className="font-bold text-xl pt-6">So who built this?</h3>
         <p className="pt-2">
-          OpenMYC was created by Kari Burt, a comedian from Houston that was trying to navigate the
-          NYC open mic scene.
+          OpenMYC was created by{' '}
+          <Link
+            href="https://kariandreah.github.io/"
+            className="text-blue-500 underline decoration-dashed hover:decoration-solid font-semibold"
+          >
+            Kari Burt
+          </Link>
+          , a comedian from Houston that was trying to navigate the NYC open mic scene.
         </p>
         <h3 className="font-bold text-xl pt-6">What is the technology stack?</h3>
         <p className="pt-2">
